c07cce354dab1d0391f3ad608a7781dbaeb6c95c
Landon Hammond <lthammond@bsu.edu>
2021-10-07 16:39:19 -0400

    Add Bedroom.tscn to start testing

---
+++ project/src/Player.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+var velocity := Vector2()
+var mass := 10
+var run_speed := 80
+var friction := 10
+
+
+func _physics_process(delta):
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+
+
+func _process(_delta):
+	velocity.x = clamp(velocity.x, -150, 150)
+
+	if Input.is_action_pressed('move_right'):
+		velocity.x += run_speed
+		$Sprite.scale.x = 8
+		if  velocity.y == 0.0:
+			pass
+			#$AnimatedSprite.play("walk 1")
+
+	if Input.is_action_pressed('move_left'):
+		velocity.x -= run_speed
+		$Sprite.scale.x = -8
+		if  velocity.y == 0.0:
+			pass
+			#$AnimatedSprite.play("walk 1")
+
+	else:
+		if velocity.x < 0.0:
+			velocity.x += friction
+		if velocity.x > 0.0:
+			velocity.x -= friction
+		if velocity == Vector2(0.0, 0.0):
+			pass
+			#$AnimatedSprite.animation = "idle 1"
