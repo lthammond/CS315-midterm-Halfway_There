42fb34de3174ab5bc941c4fe0900e7a868b8b4f5
Landon Hammond <lthammond@bsu.edu>
2021-10-07 20:34:13 -0400

    Edit backgrounds for continuity

--- project/src/Bedroom.gd
+++ project/src/Bedroom.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 func _ready():
+	if !GlobalDialogVariables.wakeUpTextRead:
+		$Dialog/WakeUpTextLabel.show()
+		GlobalDialogVariables.wakeUpTextRead = true
 	if PlayerPositionThroughDoor.door_name:
 		var door_node = find_node(PlayerPositionThroughDoor.door_name)
 		if door_node:
