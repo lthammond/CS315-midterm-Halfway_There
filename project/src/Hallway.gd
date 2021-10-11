cd9de717a7ea8ae5e264062a236a45e28cbfed6f
Landon Hammond <lthammond@bsu.edu>
2021-10-10 20:51:23 -0400

    Adjust HUD and dialogue

--- project/src/Hallway.gd
+++ project/src/Hallway.gd
@@ -5,3 +5,8 @@ func _ready():
 		var door_node = find_node(PlayerPositionThroughDoor.door_name)
 		if door_node:
 			$Player.global_position = door_node.global_position
+			
+	if !GlobalDialogVariables.hallwayTextRead:
+		$HUD/Dialog/HallwayTextLabel.show()
+		yield(get_tree().create_timer(5), "timeout")
+		$HUD/Dialog/HallwayTextLabel.hide()
