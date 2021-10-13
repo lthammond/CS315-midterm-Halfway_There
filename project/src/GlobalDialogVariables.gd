dcf9734114994f23cf834c01a1b664f1b5c90a9a
Landon Hammond <lthammond@bsu.edu>
2021-10-13 18:16:36 -0400

    Add functionality to office interactable

--- project/src/GlobalDialogVariables.gd
+++ project/src/GlobalDialogVariables.gd
@@ -4,3 +4,4 @@ var wakeUpTextRead := false
 var calendarPreviouslyTriggered := false
 var hallwayTextRead := false
 var tradingCardsRecieved := false
+var update_started := false
--- project/src/Office.gd
+++ project/src/Office.gd
@@ -5,3 +5,23 @@ func _ready():
 		var door_node = find_node(PlayerPositionThroughDoor.door_name)
 		if door_node:
 			$Player.global_position = door_node.global_position
+	if GlobalDialogVariables.update_started:
+			$Background/UpdatedScreen.show()
+
+
+func _on_Computer_start_interaction():
+	$Computer/Interaction/AnimationPlayer.play("Fade_Dark")
+	yield(get_tree().create_timer(4), "timeout")
+	$Computer/Interaction/AnimationPlayer.play("Raise_screen")
+	yield(get_tree().create_timer(3), "timeout")
+	$Computer/Interaction/ComputerSprite.texture = load("res://assets/sprites/computerUpdating.png")
+	$Computer/Interaction/ComputerSprite/UpdatePercentLabel.show()
+	$Background/UpdatedScreen.show()
+	GlobalDialogVariables.update_started = true
+	for i in range(50):
+		$Computer/Interaction/ComputerSprite/PercentageBar.rect_size.x += 1
+		$Computer/Interaction/ComputerSprite/UpdatePercentLabel.bbcode_text = "%d" % (i + 1)
+		yield(get_tree().create_timer(rand_range(0.1, 0.8)), "timeout")
+	yield(get_tree().create_timer(3), "timeout")
+	$Computer/Interaction/AnimationPlayer.play("Fade_light")
+	yield(get_tree().create_timer(6), "timeout")
