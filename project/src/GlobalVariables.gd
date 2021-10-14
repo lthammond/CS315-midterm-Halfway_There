dc6c827abb8eb82a9190fdc3254d7f4b2d4db61d
Landon Hammond <lthammond@bsu.edu>
2021-10-14 02:09:37 -0400

    Reformat Hallway scene and script

--- project/src/GlobalVariables.gd
+++ project/src/GlobalVariables.gd
@@ -1,8 +1,16 @@
 extends Node
 
+# Controls whether or not dialogue should be triggered.
 var wakeUpTextRead := false
-var underwear_deactivated := false
-var calendarPreviouslyTriggered := false
 var hallwayTextRead := false
-var tradingCardsRecieved := false
+
+# Keeps the player from switching back into underwear when reentering Bedroom.
+var underwear_deactivated := false
+
+# Keeps the player from activating an interactable more than once.
+var calendarPreviouslyTriggered := false
 var update_started := false
+
+# Makes sure prerequisites were completed before starting an interaction.
+var tradingCardsRecieved := false
+
--- project/src/Hallway.gd
+++ project/src/Hallway.gd
@@ -2,20 +2,24 @@ extends Node2D
 
 func _ready():
 	if PlayerPositionThroughDoor.door_name:
-		var door_node = find_node(PlayerPositionThroughDoor.door_name)
+		var door_node := find_node(PlayerPositionThroughDoor.door_name)
 		if door_node:
 			$Player.global_position = door_node.global_position
-			
+
+	yield(get_tree().create_timer(1), "timeout")
 	if !GlobalVariables.hallwayTextRead:
 		$HUD/Dialog/HallwayTextLabel.show()
 		GlobalVariables.hallwayTextRead = true
 
 
 func _on_CardCollection_start_interaction():
-	$CardCollection/Interaction/AnimationPlayer.play("Fade_Dark")
-	yield(get_tree().create_timer(5), "timeout")
-	$CardCollection/Interaction/AnimationPlayer.play("Raise_binder")
-	yield(get_tree().create_timer(7), "timeout")
-	$CardCollection/Interaction/AnimationPlayer.play("Fade_light")
-	yield(get_tree().create_timer(6), "timeout")
-	$HUD/Dialog/InteractionDialoge3.hide()
+	if GlobalVariables.tradingCardsRecieved:
+		$Player.active = false
+		$CardCollection/Interaction/AnimationPlayer.play("Fade_dark")
+		yield(get_tree().create_timer(5), "timeout")
+		$CardCollection/Interaction/AnimationPlayer.play("Raise_binder")
+		yield(get_tree().create_timer(7), "timeout")
+		$CardCollection/Interaction/AnimationPlayer.play("Fade_light")
+		yield(get_tree().create_timer(6), "timeout")
+		$HUD/Dialog/InteractionDialoge3.hide()
+		$Player.active = true
