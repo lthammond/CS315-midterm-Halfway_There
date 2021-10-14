2242e46020985b7fe9a4ebc4bb37f667aa1eec58
Landon Hammond <lthammond@bsu.edu>
2021-10-14 02:21:40 -0400

    Reformat HUD script

--- project/src/HUD.gd
+++ project/src/HUD.gd
@@ -1,9 +1,6 @@
 extends CanvasLayer
 
-
 func _process(_delta):
 	if GlobalVariables.calendarPreviouslyTriggered:
 		$TaskCounterLabel.text = "Tasks:%d/10" % GlobalVariables.tasks_completed
 		$TaskCounterLabel.show()
-	
-	
