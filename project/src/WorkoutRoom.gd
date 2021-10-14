5922d8ff535f306541ed1d19cb661c1217dba2ea
Landon Hammond <lthammond@bsu.edu>
2021-10-13 20:32:14 -0400

    Add interaction in workout room

--- project/src/WorkoutRoom.gd
+++ project/src/WorkoutRoom.gd
@@ -5,3 +5,21 @@ func _ready():
 		var door_node = find_node(PlayerPositionThroughDoor.door_name)
 		if door_node:
 			$Player.global_position = door_node.global_position
+
+
+func _on_FloorMat_start_interaction():
+	$FloorMat/Interaction/AnimationPlayer.play("Fade_Dark")
+	yield(get_tree().create_timer(5), "timeout")
+	for i in range(6):
+		$FloorMat/Interaction/PushupSprite.texture = load("res://assets/sprites/pushup2.png")
+		yield(get_tree().create_timer((i + 1) * 0.5), "timeout")
+		if i == 5:
+			$FloorMat/Interaction/PushupSprite.texture = load("res://assets/sprites/pushup3.png")
+		else:
+			$FloorMat/Interaction/PushupSprite.texture = load("res://assets/sprites/pushup1.png")
+			$FloorMat/Interaction/PushupCounter.bbcode_text = "%d!" % (i + 1)
+			$FloorMat/Interaction/PushupCounter.show()
+			yield(get_tree().create_timer(1.5), "timeout")
+			$FloorMat/Interaction/PushupCounter.hide()
+	yield(get_tree().create_timer(3), "timeout")
+	$FloorMat/Interaction/AnimationPlayer.play("Fade_light")
