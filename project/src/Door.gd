8fdacd15a9dd96d1db1a34523973c0b34ce670ce
Landon Hammond <lthammond@bsu.edu>
2021-10-14 01:52:19 -0400

    Reformat Door scene and script

--- project/src/Door.gd
+++ project/src/Door.gd
@@ -4,8 +4,8 @@ export(String, FILE, "*.tscn,*.scn") var target_scene
 
 func _ready():
 	$ScreenFader/ColorRect/AnimationPlayer.play("Fade_in")
-	
-	
+
+
 func _input(event):
 	if event.is_action_pressed("interact"):
 		if get_overlapping_bodies().size() > 0:
@@ -13,9 +13,9 @@ func _input(event):
 
 
 func next_level():
-	var next_scene = get_tree().change_scene(target_scene)
+	var next_scene := get_tree().change_scene(target_scene)
 	
 	if next_scene != OK:
 		print("Something went wrong while switching scenes")
-	
+
 	PlayerPositionThroughDoor.door_name = name
