6d7bb7191a1f59f14ef6d9b6b414fdc5a78a2184
Landon Hammond <lthammond@bsu.edu>
2021-10-14 13:37:24 -0400

    Added timer based on player feedback to disallow accidental spamming of interactions

--- project/src/Interactable.gd
+++ project/src/Interactable.gd
@@ -3,6 +3,7 @@ extends Area2D
 signal start_interaction
 
 func _input(event):
-	if event.is_action_pressed("interact"):
+	if event.is_action_pressed("interact") and $Timer.time_left == 0:
 		if get_overlapping_bodies().size() > 0:
 			emit_signal("start_interaction")
+			$Timer.start()
