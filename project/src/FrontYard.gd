60118799c7cbff10b8975071f9e63cac42ebe84c
Landon Hammond <lthammond@bsu.edu>
2021-10-14 01:57:52 -0400

    Reformat Front Yard scene and script

--- project/src/FrontYard.gd
+++ project/src/FrontYard.gd
@@ -2,14 +2,16 @@ extends Node2D
 
 func _ready():
 	if PlayerPositionThroughDoor.door_name:
-		var door_node = find_node(PlayerPositionThroughDoor.door_name)
+		var door_node := find_node(PlayerPositionThroughDoor.door_name)
 		if door_node:
 			$Player.global_position = door_node.global_position
 
 
 func _on_Mailbox_start_interaction():
-	$Mailbox/Interaction/AnimationPlayer.play("Fade_Dark")
+	$Player.active = false
+	$Mailbox/Interaction/AnimationPlayer.play("Fade_dark")
 	yield(get_tree().create_timer(7), "timeout")
 	$Mailbox/Interaction/AnimationPlayer.play("Fade_light")
 	yield(get_tree().create_timer(4.5), "timeout")
 	GlobalVariables.tradingCardsRecieved = true
+	$Player.active = true
