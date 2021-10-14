2c0d5176a35f8a401922527d6508e6de42855ce8
Landon Hammond <lthammond@bsu.edu>
2021-10-14 02:40:50 -0400

    Reformat Main Menu scene and script

--- project/src/MainMenu.gd
+++ project/src/MainMenu.gd
@@ -3,9 +3,11 @@ extends Control
 func _ready():
 	$AnimationPlayer.play("Drop_title")
 
+
 func _on_PlayButton_pressed():
 	$Background.texture = load("res://assets/backgrounds/menubackground2.png")
 	$AnimationPlayer.play("Fade_out")
 	yield(get_tree().create_timer(3), "timeout")
 	if get_tree().change_scene("res://src/Bedroom.tscn") != OK:
-		print ("An unexpected error occured while trying to switch to Bedroom scene")
+		print ("An unexpected error occured while trying to switch to" + 
+				"Bedroom scene")
